6ecca586d2bf97650b1d40c681972f0cce0b3fce
Marinerbyte <yasinkarani14@gmail.com>
2026-02-04 12:44:34 +0530

    Update mediasoup.js

--- core/mediasoup.js
+++ core/mediasoup.js
@@ -12,29 +12,18 @@ class MediasoupHandler {
         this.device = new mediasoup.Device({ handlerName: 'Chrome74' });
         await this.device.load({ routerRtpCapabilities: data.routerRtpCapabilities });
       }
-
       const transport = this.device.createSendTransport(data.transports.send);
-
       transport.on('connect', ({ dtlsParameters }, callback) => {
-        this.ws.send({
-          handler: 'audioroom', action: 'connect-transport',
-          roomId: data.roomId, direction: 'send', dtlsParameters
-        });
+        this.ws.send({ handler: 'audioroom', action: 'connect-transport', roomId: data.roomId, direction: 'send', dtlsParameters });
         callback();
       });
-
       transport.on('produce', ({ kind, rtpParameters }, callback) => {
-        this.ws.send({
-          handler: 'audioroom', action: 'produce',
-          roomId: data.roomId, kind, rtpParameters, requestId: "bot-mic-" + Date.now()
-        });
+        this.ws.send({ handler: 'audioroom', action: 'produce', roomId: data.roomId, kind, rtpParameters, requestId: "mic-" + Date.now() });
         this.ws.once('mic_live', (id) => callback({ id }));
       });
-
       const audio = require('./audio');
       await transport.produce({ track: audio.track });
-      console.log(`[SUCCESS] Mic is Active in ${data.roomId}`);
-    } catch (e) { console.error("Mediasoup Error:", e); }
+    } catch (e) { console.error(e); }
   }
 }
 
