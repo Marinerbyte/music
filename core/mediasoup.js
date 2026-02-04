b88c6635bb700e92969a3b7c7938ba0633b1edd0
Marinerbyte <yasinkarani14@gmail.com>
2026-02-04 12:39:33 +0530

    Create mediasoup.js

---
+++ core/mediasoup.js
@@ -0,0 +1,41 @@
+const mediasoup = require('mediasoup-client');
+
+class MediasoupHandler {
+  constructor(ws) {
+    this.ws = ws;
+    this.device = null;
+  }
+
+  async init(data) {
+    try {
+      if (!this.device) {
+        this.device = new mediasoup.Device({ handlerName: 'Chrome74' });
+        await this.device.load({ routerRtpCapabilities: data.routerRtpCapabilities });
+      }
+
+      const transport = this.device.createSendTransport(data.transports.send);
+
+      transport.on('connect', ({ dtlsParameters }, callback) => {
+        this.ws.send({
+          handler: 'audioroom', action: 'connect-transport',
+          roomId: data.roomId, direction: 'send', dtlsParameters
+        });
+        callback();
+      });
+
+      transport.on('produce', ({ kind, rtpParameters }, callback) => {
+        this.ws.send({
+          handler: 'audioroom', action: 'produce',
+          roomId: data.roomId, kind, rtpParameters, requestId: "bot-mic-" + Date.now()
+        });
+        this.ws.once('mic_live', (id) => callback({ id }));
+      });
+
+      const audio = require('./audio');
+      await transport.produce({ track: audio.track });
+      console.log(`[SUCCESS] Mic is Active in ${data.roomId}`);
+    } catch (e) { console.error("Mediasoup Error:", e); }
+  }
+}
+
+module.exports = MediasoupHandler;
